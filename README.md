44c754c67326d7536f91e877b35c173303462953
fuadsuleyman <fuadsuleymanov@outlook.com>
2021-12-20 13:46:46 +0400

    ready simple graphql app

---
+++ README.md
@@ -0,0 +1,8 @@
+### this project created by follow below blog post
+https://www.inovex.de/de/blog/graphql-application-golang-tutorial/
+
+##### after change schema.graphqls or gplgen.yml file run below in terminal
+go run github.com/99designs/gqlgen generate
+
+### for run project just as below
+go run server.gogit commit -m "first commit"git
---
+++ graph/resolver.go
@@ -0,0 +1,31 @@
+package graph
+
+import "graphql-go-example/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+func NewResolver() *Resolver {
+
+	users := make([]*model.User, 0)
+
+	users = append(users, &model.User{ID: "1", Name: "fuad"})
+
+	users = append(users, &model.User{ID: "2", Name: "laura"})
+
+	return &Resolver{
+		todos:      make([]*model.Todo, 0),
+		users:      users,
+		lastUserId: 3,
+		usersChan:  make(chan *model.User),
+	}
+}
+
+type Resolver struct {
+	todos      []*model.Todo
+	users      []*model.User
+	lastTodoId int
+	lastUserId int
+	usersChan  chan *model.User
+}
